55b785a75ab1ebb9a9675bd58e360974956799ec
Esesosa-max <75207794+Esesosa-max@users.noreply.github.com>
2021-11-08 06:42:09 +0100

    Add files via upload

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>SS1 Notes</title>
+</svelte:head>
+<h4>Welcome To The ss1b Note App</h4>
+<p>Made by Idukpaye Osazuwa Alex who is in SSS1B</p> 
+
+<h5>To see All Notes</h5>
+<p>Click All Notes</p>
+<script>
+
+
+</script>
